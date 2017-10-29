ab57bdad3b724a82623edd9ff29769677a2693dd
Pär Nygren <par.a.nygren@gmail.com>
2017-10-29 15:05:06 +0100

    Add project files.

---
+++ Azure-aspnetmvc/Global.asax
@@ -0,0 +1,1 @@
+﻿<%@ Application Codebehind="Global.asax.cs" Inherits="Azure_aspnetmvc.MvcApplication" Language="C#" %>
---
+++ Azure-aspnetmvc/Startup.cs
@@ -0,0 +1,14 @@
+﻿using Microsoft.Owin;
+using Owin;
+
+[assembly: OwinStartupAttribute(typeof(Azure_aspnetmvc.Startup))]
+namespace Azure_aspnetmvc
+{
+    public partial class Startup
+    {
+        public void Configuration(IAppBuilder app)
+        {
+            ConfigureAuth(app);
+        }
+    }
+}
